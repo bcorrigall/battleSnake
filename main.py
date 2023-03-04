94d570da20168a6ad2f5c393572a8f91072ab49d
bran8621 <bran8621@gmail.com>
2023-03-04 13:04:29 -0500

    AStarSnake Initial

--- main.py
+++ main.py
@@ -1,121 +1,267 @@
-# Welcome to
-# __________         __    __  .__                               __
-# \______   \_____ _/  |__/  |_|  |   ____   ______ ____ _____  |  | __ ____
-#  |    |  _/\__  \\   __\   __\  | _/ __ \ /  ___//    \\__  \ |  |/ // __ \
-#  |    |   \ / __ \|  |  |  | |  |_\  ___/ \___ \|   |  \/ __ \|    <\  ___/
-#  |________/(______/__|  |__| |____/\_____>______>___|__(______/__|__\\_____>
-#
-# This file can be a nice home for your Battlesnake logic and helper functions.
-#
-# To get you started we've included code to prevent your Battlesnake from moving backwards.
-# For more info see docs.battlesnake.com
 
-import random
-import typing
+from astar import *
+import bottle
+import copy
+import math
+import os
+
+SNAKE_BUFFER = 3
+ID = 'de508402-17c8-4ac7-ab0b-f96cb53fbee8'
+SNAKE = 1
+WALL = 2
+FOOD = 3
+GOLD = 4
+SAFETY = 5
 
 
-# info is called when you create your Battlesnake on play.battlesnake.com
-# and controls your Battlesnake's appearance
-# TIP: If you open your Battlesnake URL in a browser you should see this data
-def info() -> typing.Dict:
-    print("INFO")
+def goals(data):
+    result = data['food']
+    if data['mode'] == 'advanced':
+        result.extend(data['gold'])
+    return result
+
+
+def direction(from_cell, to_cell):
+    dx = to_cell[0] - from_cell[0]
+    dy = to_cell[1] - from_cell[1]
+
+    if dx == 1:
+        return 'east'
+    elif dx == -1:
+        return 'west'
+    elif dy == -1:
+        return 'north'
+    elif dy == 1:
+        return 'south'
+
+
+def distance(p, q):
+    dx = abs(p[0] - q[0])
+    dy = abs(p[1] - q[1])
+    return dx + dy
+
+
+def closest(items, start):
+    closest_item = None
+    closest_distance = 10000
+
+    # TODO: use builtin min for speed up
+    for item in items:
+        item_distance = distance(start, item)
+        if item_distance < closest_distance:
+            closest_item = item
+            closest_distance = item_distance
+
+    return closest_item
+
+
+def init(data):
+    grid = [[0 for col in xrange(data['height'])]
+            for row in xrange(data['width'])]
+    for SNAKE in data['snakes']:
+        if SNAKE['id'] == ID:
+            mysnake = SNAKE
+        for coord in SNAKE['coords']:
+            grid[coord[0]][coord[1]] = SNAKE
+
+    if data['mode'] == 'advanced':
+        for wall in data['walls']:
+            grid[wall[0]][wall[1]] = WALL
+        for g in data['gold']:
+            grid[g[0]][g[1]] = GOLD
+
+    for f in data['food']:
+        grid[f[0]][f[1]] = FOOD
+
+    return mysnake, grid
+
+
+@bottle.route('/static/<path:path>')
+def static(path):
+    return bottle.static_file(path, root='static/')
+
+
+@bottle.get('/')
+def index():
+    head_url = '%s://%s/static/Traitor.gif' % (
+        bottle.request.urlparts.scheme,
+        bottle.request.urlparts.netloc
+    )
 
     return {
-        "apiversion": "1",
-        "author": "Snakes on a Plane",  # TODO: Your Battlesnake Username
-        "color": "#FF0000",  # TODO: Choose color
-        "head": "default",  # TODO: Choose head
-        "tail": "default",  # TODO: Choose tail
+        'color': '#00ff00',
+        'head': head_url
     }
 
 
-# start is called when your Battlesnake begins a game
-def start(game_state: typing.Dict):
-    print("GAME START")
+@bottle.post('/start')
+def start():
+    data = bottle.request.json
+
+    # TODO: Do things with data
+
+    return {
+        'taunt': 'battlesnake-python!'
+    }
+# DATA OBJECT
+# {
+#     "game": "hairy-cheese",
+#     "mode": "advanced",
+#     "turn": 4,
+#     "height": 20,
+#     "width": 30,
+#     "snakes": [
+#         <Snake Object>, <Snake Object>, ...
+#     ],
+#     "food": [
+#         [1, 2], [9, 3], ...
+#     ],
+#     "walls": [    // Advanced Only
+#         [2, 2]
+#     ],
+#     "gold": [     // Advanced Only
+#         [5, 5]
+#     ]
+# }
+
+# SNAKE
+# {
+#     "id": "1234-567890-123456-7890",
+#     "name": "Well Documented Snake",
+#     "status": "alive",
+#     "message": "Moved north",
+#     "taunt": "Let's rock!",
+#     "age": 56,
+#     "health": 83,
+#     "coords": [ [1, 1], [1, 2], [2, 2] ],
+#     "kills": 4,
+#     "food": 12,
+#     "gold": 2
+# }
 
 
-# end is called when your Battlesnake finishes a game
-def end(game_state: typing.Dict):
-    print("GAME OVER\n")
+@bottle.post('/move')
+def move():
+    data = bottle.request.json
+    SNAKE, grid = init(data)
+
+    # foreach snake
+    for enemy in data['snakes']:
+        if (enemy['id'] == ID):
+            continue
+        if distance(SNAKE['coords'][0], enemy['coords'][0]) > SNAKE_BUFFER:
+            continue
+        if (len(enemy['coords']) > len(SNAKE['coords'])-1):
+            # dodge
+            if enemy['coords'][0][1] < data['height']-1:
+                grid[enemy['coords'][0][0]][enemy['coords'][0][1]+1] = SAFETY
+            if enemy['coords'][0][1] > 0:
+                grid[enemy['coords'][0][0]][enemy['coords'][0][1]-1] = SAFETY
+
+            if enemy['coords'][0][0] < data['width']-1:
+                grid[enemy['coords'][0][0]+1][enemy['coords'][0][1]] = SAFETY
+            if enemy['coords'][0][0] > 0:
+                grid[enemy['coords'][0][0]-1][enemy['coords'][0][1]] = SAFETY
+
+    SNAKE_head = SNAKE['coords'][0]
+    SNAKE_coords = SNAKE['coords']
+    path = None
+    middle = [data['width'] / 2, data['height'] / 2]
+    foods = sorted(data['food'], key=lambda p: distance(p, middle))
+    if data['mode'] == 'advanced':
+        foods = data['gold'] + foods
+    for food in foods:
+        # print food
+        tentative_path = a_star(SNAKE_head, food, grid, SNAKE_coords)
+        if not tentative_path:
+            # print "no path to food"
+            continue
+
+        path_length = len(tentative_path)
+        SNAKE_length = len(SNAKE_coords) + 1
+
+        dead = False
+        for enemy in data['snakes']:
+            if enemy['id'] == ID:
+                continue
+            if path_length > distance(enemy['coords'][0], food):
+                dead = True
+        if dead:
+            continue
+
+        # Update SNAKE
+        if path_length < SNAKE_length:
+            remainder = SNAKE_length - path_length
+            new_SNAKE_coords = list(
+                reversed(tentative_path)) + SNAKE_coords[:remainder]
+        else:
+            new_SNAKE_coords = list(reversed(tentative_path))[:SNAKE_length]
+
+        if grid[new_SNAKE_coords[0][0]][new_SNAKE_coords[0][1]] == FOOD:
+            # we ate food so we grow
+            new_SNAKE_coords.append(new_SNAKE_coords[-1])
+
+        # Create a new grid with the updates SNAKE positions
+        new_grid = copy.deepcopy(grid)
+
+        for coord in SNAKE_coords:
+            new_grid[coord[0]][coord[1]] = 0
+        for coord in new_SNAKE_coords:
+            new_grid[coord[0]][coord[1]] = SNAKE
+
+        # printg(grid, 'orig')
+        # printg(new_grid, 'new')
+
+        # print SNAKE['coords'][-1]
+        foodtotail = a_star(
+            food, new_SNAKE_coords[-1], new_grid, new_SNAKE_coords)
+        if foodtotail:
+            path = tentative_path
+            break
+        # print "no path to tail from food"
+
+    if not path:
+        path = a_star(SNAKE_head, SNAKE['coords'][-1], grid, SNAKE_coords)
+
+    despair = not (path and len(path) > 1)
+
+    if despair:
+        for neighbour in neighbours(SNAKE_head, grid, 0, SNAKE_coords, [1, 2, 5]):
+            path = a_star(SNAKE_head, neighbour, grid, SNAKE_coords)
+            # print 'i\'m scared'
+            break
+
+    despair = not (path and len(path) > 1)
+
+    if despair:
+        for neighbour in neighbours(SNAKE_head, grid, 0, SNAKE_coords, [1, 2]):
+            path = a_star(SNAKE_head, neighbour, grid, SNAKE_coords)
+            # print 'lik so scared'
+            break
+
+    if path:
+        assert path[0] == tuple(SNAKE_head)
+        assert len(path) > 1
+
+    return {
+        'move': direction(path[0], path[1]),
+        'taunt': 'TRAITOR!'
+    }
 
 
-# move is called on every turn and returns your next move
-# Valid moves are "up", "down", "left", or "right"
-# See https://docs.battlesnake.com/api/example-move for available data
-def move(game_state: typing.Dict) -> typing.Dict:
+@bottle.post('/end')
+def end():
+    data = bottle.request.json
 
-    is_move_safe = {"up": True, "down": True, "left": True, "right": True}
+    # TODO: Do things with data
 
-    # We've included code to prevent your Battlesnake from moving backwards
-    my_head = game_state["you"]["body"][0]  # Coordinates of your head
-    my_neck = game_state["you"]["body"][1]  # Coordinates of your "neck"
-
-    if my_neck["x"] < my_head["x"]:  # Neck is left of head, don't move left
-        is_move_safe["left"] = False
-
-    elif my_neck["x"] > my_head["x"]:  # Neck is right of head, don't move right
-        is_move_safe["right"] = False
-
-    elif my_neck["y"] < my_head["y"]:  # Neck is below head, don't move down
-        is_move_safe["down"] = False
-
-    elif my_neck["y"] > my_head["y"]:  # Neck is above head, don't move up
-        is_move_safe["up"] = False
-
-    # TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
-    board_width = game_state['board']['width']
-    board_height = game_state['board']['height']
-
-    if my_head["x"] == 0:
-        is_move_safe["left"] = False
-    elif my_head["x"]+1 == board_width:
-        is_move_safe["right"] = False
-
-    if my_head["y"] == 0:
-        is_move_safe["down"] = False
-    elif my_head["y"]+1 == board_height:
-        is_move_safe["up"] = False
+    return {
+        'taunt': 'Boa Down!'
+    }
 
 
-    # TODO: Step 2 - Prevent your Battlesnake from colliding with itself
-    my_body = game_state['you']['body']
-    
-    if {"x" : my_head["x"]+1, "y" : my_head["y"]} in my_body:
-        is_move_safe["right"] = False
-    if {"x" : my_head["x"]-1, "y" : my_head["y"]} in my_body:
-        is_move_safe["left"] = False
-    if {"x" : my_head["x"], "y" : my_head["y"]+1} in my_body:
-        is_move_safe["up"] = False
-    if {"x" : my_head["x"], "y" : my_head["y"]-1} in my_body:
-        is_move_safe["down"] = False
-
-
-
-    # TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
-    # opponents = game_state['board']['snakes']
-
-    # Are there any safe moves left?
-    safe_moves = []
-    for move, isSafe in is_move_safe.items():
-        if isSafe:
-            safe_moves.append(move)
-
-    if len(safe_moves) == 0:
-        print(f"MOVE {game_state['turn']}: No safe moves detected! Moving down")
-        return {"move": "down"}
-
-    # Choose a random move from the safe ones
-    next_move = random.choice(safe_moves)
-
-    # TODO: Step 4 - Move towards food instead of random, to regain health and survive longer
-    # food = game_state['board']['food']
-
-    print(f"MOVE {game_state['turn']}: {next_move}")
-    return {"move": next_move}
-
-
-# Start server when `python main.py` is run
-if __name__ == "__main__":
-    from server import run_server
-
-    run_server({"info": info, "start": start, "move": move, "end": end})
+# Expose WSGI app (so gunicorn can find it)
+application = bottle.default_app()
+if __name__ == '__main__':
+    bottle.run(application, host=os.getenv(
+        'IP', '0.0.0.0'), port=os.getenv('PORT', '8080'))
